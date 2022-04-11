f91d982939304d32ebe31ea468e3d2fd6bba591a
Rael N. Pires <rnogpiresfinancial@gmail.com>
2022-04-11 18:19:44 -0300

    feat: implemented Categories method at models

---
+++ models/categories.js
@@ -0,0 +1,12 @@
+const Categories = (sequelize, DataTypes) => {
+  const categories = sequelize.define('Categories', {
+    name: DataTypes.STRING,
+  }, {
+    timestamps: false,
+    tableName: 'Categories',
+  });
+
+  return categories;
+};
+
+module.exports = Categories;
