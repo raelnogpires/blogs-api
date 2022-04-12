1fac9c3517ae9edfd98394569d58ac2e2d35c1e7
Rael N. Pires <rnogpiresfinancial@gmail.com>
2022-04-12 18:31:00 -0300

    fix: errorMiddleware require on index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const userRouter = require('./routers/user');
-const errorMiddleware = require('./middlewares/error');
+const { errorMiddleware } = require('./middlewares/error');
 
 const app = express();
 
