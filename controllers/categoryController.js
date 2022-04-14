db289e9e507d112e6bf39a95714eb8ad4f2b308e
Rael N. Pires <rnogpiresfinancial@gmail.com>
2022-04-14 18:10:24 -0300

    feat: created categoryController.create

---
+++ controllers/categoryController.js
@@ -0,0 +1,16 @@
+const categoryService = require('../services/categoryService');
+const statusCode = require('../middlewares/httpStatusCode');
+
+const create = async (req, res) => {
+  const { name } = req.body;
+
+  if (!name) {
+    return res.status(statusCode.BAD_REQUEST).json({ message: '"name" is required' });
+  }
+
+  await categoryService.create(name);
+
+  return res.status(statusCode.OK).json({ name });
+};
+
+module.exports = { create };
--- services/categoryService.js
+++ services/categoryService.js
@@ -2,7 +2,6 @@ const { Categories } = require('../models');
 
 const create = async (name) => {
   await Categories.create({ name });
-  return true;
 };
 
 module.exports = { create };
